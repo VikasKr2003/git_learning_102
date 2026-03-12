cc1b7bce713678989ff88bbedf015b40aa11902c
vikas <vikaskr658@gmail.com>
2026-03-12 21:14:48 +0530

    Student class created

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Studnet{
+    String name;
+    int age;
+    String grade;
+
+    public Studnet(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+}
